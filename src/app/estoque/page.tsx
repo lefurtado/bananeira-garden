e3b4f256f1fd6f2e399043a00ae662ea40096bc7
Leandro <le_furtado@outlook.com.br>
2024-06-02 03:34:34 -0300

    ✨ feat(data-table): add data-table

--- src/app/estoque/page.tsx
+++ src/app/estoque/page.tsx
@@ -1,3 +1,4 @@
+import { Payment, columns } from "./columns";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -6,8 +7,23 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
+import { DataTable } from "@/components/ui/data-table";
 
-export default function Estoque() {
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    // ...
+  ];
+}
+
+export default async function Estoque() {
+  const data = await getData();
   return (
     <div>
       <Breadcrumb>
@@ -21,6 +37,7 @@ export default function Estoque() {
           </BreadcrumbItem>
         </BreadcrumbList>
       </Breadcrumb>
+      <DataTable columns={columns} data={data} />
     </div>
   );
 }
